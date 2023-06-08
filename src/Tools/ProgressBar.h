36b4ba35668e320952e3b4b5fd51624b1eacd4ae
Aurélien HURAND <aurelien.hurand@gmail.com>
2023-06-09 00:49:44 +0200

    Progress on progressBar

---
+++ src/Tools/ProgressBar.h
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <chrono>
+#pragma once
+
+namespace Tools
+{
+    class ProgressBar
+    {
+    public:
+        ProgressBar(std::string name);
+        ~ProgressBar();
+        void InitProgress();
+        void EndProgress();
+        int GetConsoleWidth();
+        std::string name;
+        float progress;
+    protected:
+        void PrintProgressPart(int size);
+    };
+
+    class ClasProBar : public ProgressBar
+    {
+    public:
+        ClasProBar(std::string name, float maxValue);
+        ~ClasProBar();
+        void ChangeProgress(float value);
+        void EndProgress();
+        void InitProgress();
+    private:
+        void PrintProgressBar(float newValue);
+        float maxValue;
+        float progress;
+
+    };
+
+
+
+    class NetProBar : public ProgressBar
+    {
+    public:
+        NetProBar(std::string name, int totalBytes);
+        ~NetProBar();
+        void ChangeProgress(uint64_t ByteSent);
+        void EndProgress();
+        void InitProgress();
+
+    private:
+        std::chrono::_V2::high_resolution_clock::time_point lastTime = std::chrono::high_resolution_clock::now();
+        void PrintProgressBar(float newProgress);
+        uint64_t byteDiff = 0;
+        uint64_t bytesToDownload;
+    };
+
+    class TrainBar : public ProgressBar
+    {
+    public:
+        TrainBar(int totalEpochs);
+        void ChangeProgress(int EpochsDone, float loss);
+    private:
+        void Print();
+        int epochs;
+        float loss;
+        int totalEpochs;
+    };
+
+
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -8,41 +8,11 @@
 #include "InputLayer.h"
 #include "Loss.h"
 #include "Examples/Mnist.h"
+#include "Examples/Tests.h"
 #include <bits/stdc++.h>
 
 
 int main()
 {
-    time_t start, end;
-    time(&start);
 
-
-
-    Network network = Network();
-    network.AddLayer(new InputLayer(2));
-    network.AddLayer(new FCL(100, new Sigmoid()));
-    network.AddLayer(new LastLayer(2, new Softmax(), new CrossEntropy()));
-
-    network.Compile();
-
-    network.PrintNetwork();
-
-
-    Matrix** input = new Matrix*[2];
-    Matrix** output = new Matrix*[2];
-    input[0] = new Matrix(2,1,new double[2]{1,0});
-    input[1] = new Matrix(2,1,new double[2]{0,1});
-    output[0] = new Matrix(2,1,new double[2]{1,0});
-    output[1] = new Matrix(2,1,new double[2]{0,1});
-    std::cout << *network.FeedForward(input[0]) << std::endl;
-    std::cout << *network.FeedForward(input[1]) << std::endl;
-    network.Learn(10000,0.1,input,output,1,2,1);
-    std::cout << *input[0] << std::endl;
-    std::cout << *network.FeedForward(input[0]) << std::endl;
-    std::cout << *network.FeedForward(input[1]) << std::endl;
-
-    time(&end);
-    double time_taken = double(end) - double(start);
-    std::cout << "Time taken by program is : " << std::fixed
-         << time_taken << std::setprecision(5) << " sec " << std::endl;
 }
