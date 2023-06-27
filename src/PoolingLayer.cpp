f69f7231d1d8883b8f91eefa2550ca8802d1c4a7
Mathieu BREBION <mathieu.brebion@epita.fr>
2023-06-27 16:16:14 +0200

    Small mistake corrected

--- src/PoolingLayer.cpp
+++ src/PoolingLayer.cpp
@@ -18,7 +18,7 @@ void PoolingLayer::UpdateWeights(double learningRate, int batchSize)
 
 void PoolingLayer::Compile(LayerShape* previousActivation)
 {
-    layerShape = new LayerShape(previousActivation->dimensions[0] / stride + 1, previousActivation->dimensions[1] / stride + 1, previousOutput->dimensions[2]);
+    layerShape = new LayerShape(previousActivation->dimensions[0] / stride + 1, previousActivation->dimensions[1] / stride + 1, previousActivation->dimensions[2]);
     result = new Matrix(layerShape->dimensions[0], layerShape->dimensions[1]);
     newDelta = new Matrix(previousActivation->dimensions[0], previousActivation->dimensions[1], (double)0);
 }
