eabe95a33318eefa9d8a7c03adc6194c5e817a21
Mathieu BREBION <mathieu.brebion@epita.fr>
2023-07-22 20:11:14 +0200

    fixed quickraw loading

--- src/Examples/Quickdraw.cpp
+++ src/Examples/Quickdraw.cpp
@@ -50,7 +50,7 @@ void QuickDraw1(const int numDrawingsPerCategory)
                                          false);
     std::cout << "loaded" << std::endl;
 
-    Network* network = new Network();
+    auto* network = new Network();
     network->AddLayer(new InputLayer(784));
     network->AddLayer(new FCL(128, new ReLU()));
     network->AddLayer(new FCL(numCategories, new Softmax()));
@@ -60,8 +60,7 @@ void QuickDraw1(const int numDrawingsPerCategory)
 
     int trainLength = dataLength * 0.8;
     int testLength = dataLength - trainLength;
-    DataLoader* dataLoader = new DataLoader(data, trainLength);
-    dataLoader->Shuffle();
+    auto* dataLoader = new DataLoader(data, trainLength);
     network->Learn(20, 0.01, dataLoader, 64, 16);
 
     double trainingAccuracy = TestAccuracy(network, data, 1000);
@@ -81,7 +80,7 @@ void QuickDraw2(const int numDrawingsPerCategory)
     Matrix*** data = GetQuickdrawDataset("datasets/Quickdraw", dataLength, numCategories, numDrawingsPerCategory, true);
     std::cout << "loaded" << std::endl;
 
-    Network* network = new Network();
+    auto* network = new Network();
     network->AddLayer(new InputLayer(28, 28, 1));
     network->AddLayer(new ConvLayer(new LayerShape(3, 3, 32), new ReLU()));
     network->AddLayer(new MaxPoolLayer(2, 2));
@@ -93,9 +92,9 @@ void QuickDraw2(const int numDrawingsPerCategory)
     network->PrintNetwork();
     int trainLength = dataLength * 0.8;
     int testLength = dataLength - trainLength;
-    DataLoader* dataLoader = new DataLoader(data, trainLength);
+    auto* dataLoader = new DataLoader(data, trainLength);
     dataLoader->Shuffle();
-    network->Learn(3, 0.01, dataLoader, 64, 16);
+    network->Learn(10, 0.01, dataLoader, 96, 16);
 
     double trainingAccuracy = TestAccuracy(network, data, 1000);
     std::cout << "Training Accuracy : " << trainingAccuracy * 100 << "% \n";
@@ -111,7 +110,7 @@ GetQuickdrawDataset(const std::string& path, int dataLength, int numCategories, 
 {
     using recursive_directory_iterator = std::filesystem::recursive_directory_iterator;
 
-    Matrix*** dataset = new Matrix** [dataLength];
+    auto*** dataset = new Matrix** [dataLength];
 
     int inputIndex = 0, categoryIndex = 0;
     const int rows = format2D ? 28 : 28 * 28;
@@ -125,9 +124,26 @@ GetQuickdrawDataset(const std::string& path, int dataLength, int numCategories, 
         std::ifstream in(dirEntry.path(), std::ios::binary | std::ios::in);
         int numDrawingsInFile = numDrawingsPerCategory;
         //in.read((char*)(&numDrawingsInFile), sizeof(numDrawingsInFile));
+        in.seekg(sizeof(int), std::ios::beg);
 
         for (int i = 0; i < numDrawingsInFile; i++)
         {
+            /*if (inputIndex % (2500) == 0)
+            {
+                const int pos = in.tellg();
+                std::cout << dirEntry.path().filename() << std::endl;
+                for (int y = 0; y < 28; ++y)
+                {
+                    for (int x = 0; x < 28; ++x)
+                    {
+                        auto* a = new unsigned char(0);
+                        in.read((char*) a, 1);
+                        std::cout << std::setw(3) << (int) *a << " ";
+                    }
+                    std::cout << std::endl;
+                }
+                in.seekg(pos);
+            }*/
             dataset[inputIndex] = new Matrix* [2];
             dataset[inputIndex][1] = LabelToMatrix(categoryIndex, numCategories);
             dataset[inputIndex][0] = new Matrix(rows, cols);
@@ -146,12 +162,17 @@ GetQuickdrawDataset(const std::string& path, int dataLength, int numCategories, 
         in.close();
     }
 
+    // Shuffle the dataset
+    std::random_device rd;
+    auto rng = std::mt19937(rd());
+    std::shuffle(dataset, dataset + dataLength, rng);
+
     return dataset;
 }
 
 Matrix* LabelToMatrix(const int label, const int numLabels)
 {
-    Matrix* res = new Matrix(numLabels, 1);
+    auto* res = new Matrix(numLabels, 1);
     (*res)[label] = 1;
 
     return res;
