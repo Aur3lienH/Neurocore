6b2237c6d6915da26602b51ce15cb5907ea6740a
Aurélien HURAND <aurelien.hurand@gmail.com>
2023-08-21 00:56:25 +0200

    started the building of the horonzital first matrix

---
+++ src/MatrixOperationsOriented.cpp
@@ -0,0 +1,12 @@
+#include "Matrix.h"
+#include "Operations.h"
+#include <iostream>
+#include <vector>
+
+
+std::vector<Operation*> Matrix::O_CrossProduct(Matrix* a, Matrix* b, Matrix* output)
+{
+    std::vector<Operation*> res;
+    
+    
+}
