d9dd55eb6d9320599bb59c0391103a435c8e26f1
Mathieu Brebion <mathieu.brebion@epita.fr>
2024-12-20 18:59:28 +0100

    tried to do stuff with templates

--- include/network/layers/InputLayer.cuh
+++ include/network/layers/InputLayer.cuh
@@ -4,7 +4,8 @@
 #include "network/layers/Layer.cuh"
 #include "network/LayerShape.cuh"
 
-class InputLayer : public Layer
+template<LayerShape l>
+class InputLayer
 {
 public:
     explicit InputLayer(int inputSize);
@@ -25,13 +26,13 @@ public:
 
     void UpdateWeights(double learningRate, int batchSize);
 
-    void AddDeltaFrom(Layer* otherLayer);
+    void AddDeltaFrom(Layer<InputLayer>* otherLayer);
 
     void Compile(LayerShape* layerShape);
 
     std::string getLayerTitle();
 
-    Layer* Clone();
+    Layer<InputLayer>* Clone();
 
     static InputLayer* Load(std::ifstream& reader);
 
@@ -41,7 +42,106 @@ private:
     const MAT* input = nullptr;
 
     void (* FeedFunc)(const MAT*, Matrix*, int);
+
+    LayerShape* layerShape;
+    Optimizer* optimizer = nullptr;
 };
 
+template<LayerShape* layershape>
+Layer<InputLayer<LayerShape>>::InputLayer(const int inputSize)
+{
+    layerShape = new LayerShape(inputSize);
+}
+
+template<LayerShape* layershape>
+Layer<InputLayer<LayerShape>::InputLayer(const int rows, const int cols, const int size)
+{
+    layerShape = new LayerShape(rows, cols, size);
+}
+
+template<LayerShape* layershape>
+InputLayer<layershape>::InputLayer(LayerShape* LayerShape)
+{
+    this->layerShape = LayerShape;
+}
+
+template<LayerShape* layershape>
+const MAT* InputLayer<LayerShape*>::FeedForward(const MAT* _input)
+{
+    input = _input;
+    return _input;
+}
+
+template<LayerShape* layershape>
+const MAT* InputLayer<LayerShape*>::BackPropagate(const MAT* delta, const MAT* lastWeights)
+{
+    return nullptr;
+}
+
+template<LayerShape* layershape>
+const MAT* InputLayer<LayerShape*>::getResult() const
+{
+    return input;
+}
+
+template<LayerShape* layershape>
+void InputLayer<LayerShape*>::ClearDelta()
+{
+
+}
+
+template<LayerShape* layershape>
+void InputLayer<LayerShape*>::Compile(LayerShape* layerShape)
+{
+    std::cout << "compiling Input layer\n";
+}
+
+template<LayerShape* layershape>
+void InputLayer<LayerShape*>::UpdateWeights(const double learningRate, const int batchSize)
+{
+
+}
+
+template<LayerShape* layershape>
+void InputLayer<LayerShape*>::AddDeltaFrom(Layer* otherLayer)
+{
+
+}
+
+template<LayerShape* layershape>
+std::string Layer<InputLayer<LayerShape*>>::getLayerTitle()
+{
+    std::string buf = "InputLayer\n";
+    buf += layerShape->GetDimensions() + "\n";
+    return buf;
+}
+
+template<LayerShape* layershape>
+Layer<InputLayer<LayerShape* layershape>> Layer<InputLayer<LayerShape*>>::Clone()
+{
+    if (layerShape->size == 1)
+    {
+        return new Layer<new InputLayer(layerShape->dimensions[0])>();
+    }
+    return new InputLayer(
+            new LayerShape(layerShape->dimensions[0], layerShape->dimensions[1], layerShape->dimensions[2]));
+}
 
 
+template<LayerShape* layershape>
+Layer<InputLayer<LayerShape*>>* InputLayer<LayerShape*>::Load(std::ifstream& reader)
+{
+    LayerShape* layerShape = LayerShape::Load(reader);
+    return new InputLayer(layerShape);
+}
+
+void Layer<InputLayer<LayerShape*>>::SpecificSave(std::ofstream& writer)
+{
+    layerShape->Save(writer);
+}
+
+template<LayerShape* layershape>
+void Layer<InputLayer<LayerShape*>>::AverageGradients(int batchSize)
+{
+
+}
