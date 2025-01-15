e97cc1f5c9fa93848a4438696a5f1be8c432e787
aurelien.hurand <aurelien.hurand@epita.fr>
2025-01-15 09:32:05 +0000

    feat: added src files for python lib !

---
+++ python_lib/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="deep learning lib",
+    version="0.0.1",
+    packages=find_packages(),
+)
---
+++ python_lib/src/network/Layers.py
@@ -0,0 +1,7 @@
+from python_lib.src.network.Activation import Activation
+
+
+class Layer:
+
+    def __init___(self,activation : Activation):
+        self.Activation = activation
