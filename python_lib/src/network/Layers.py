075bb3fca59e100a0622d43748c4111cd0eee06a
aurelien.hurand <aurelien.hurand@epita.fr>
2025-01-22 20:20:17 +0100

    feat: Compilation of network for training working in python, created class to compile all of the layers.

--- python_lib/src/network/Layers.py
+++ python_lib/src/network/Layers.py
@@ -40,3 +40,67 @@ class FCL(Layer):
     
     def get_layer_shape(self):
         return self.layerShape
+
+class ConvLayer(Layer):
+
+    def __init__(self, layerShape: LayerShape, activation, kernelShape: LayerShape, optimizer):
+        self.layerShape = layerShape
+        self.activation = activation
+        self.kernelShape = kernelShape
+        self.optimizer = optimizer
+
+    def get_code(self, prevLayerShape: LayerShape):
+        return f'ConvLayer<{self.activation.get_code(self.layerShape)},{prevLayerShape.get_code()},{self.layerShape.get_code()},{self.optimizer.get_code()}>'
+
+    def get_layer_shape(self):
+        return self.layerShape
+
+
+class Dropout(Layer):
+    def __init__(self, layerShape: LayerShape, rate: float):
+        self.layerShape = layerShape
+        self.rate = rate
+
+    def get_code(self, prevLayerShape: LayerShape):
+        return f'Dropout<{self.layerShape.get_code()},{self.rate}>'
+
+    def get_layer_shape(self):
+        return self.layerShape
+
+
+class Reshape(Layer):
+    def __init__(self, newShape: LayerShape , prevShape: LayerShape):
+        self.newShape = newShape
+        self.prevShape = prevShape
+
+    def get_code(self, prevLayerShape: LayerShape):
+        return f'Reshape<{self.newShape.get_code()},{self.prevShape.get_code()}>'
+
+    def get_layer_shape(self):
+        return self.newShape
+
+class AveragePooling(Layer):
+
+    def __init__(self, layerShape: LayerShape, filterSize, stride):
+        self.layerShape = layerShape
+        self.filterSize = filterSize
+        self.stride = stride
+
+    def get_code(self, prevLayerShape: LayerShape):
+        return f'AveragePooling<{self.layerShape.get_code()},{self.filterSize},{self.stride}>'
+
+    def get_layer_shape(self):
+        return self.layerShape
+
+class MaxPooling(Layer):
+
+    def __init__(self, layerShape: LayerShape, filterSize, stride):
+        self.layerShape = layerShape
+        self.filterSize = filterSize
+        self.stride = stride
+
+    def get_code(self, prevLayerShape: LayerShape):
+        return f'MaxPooling<{self.layerShape.get_code()},{self.filterSize},{self.stride}>'
+
+    def get_layer_shape(self):
+        return self.layerShape
